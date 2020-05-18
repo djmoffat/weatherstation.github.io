6ca5e84cdad46475b423d1a5034c5fa499f0b89e
Dave Moffat <me@davemoffat.com>
2020-05-18 23:29:19 +0100

    update

--- code/plotGraph.py
+++ code/plotGraph.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import datetime
 import pandas as pd
 
-data = pd.read_csv('../weather_data.csv',parse_dates=True)
+data = pd.read_csv('weather_data.csv',parse_dates=True)
 ax = data.plot()
 ax.legend(["Temperature (c)", "Humidity (%)"]);
 fig = ax.get_figure()
-plt.savefig('../images/weather.png')
+plt.savefig('images/weather.png')
