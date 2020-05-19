a56dfc00e0863a9057b3ac43ed24bd06d52bda78
Dave Moffat <me@davemoffat.com>
2020-05-19 11:30:23 +0100

    update

--- code/plotGraph.py
+++ code/plotGraph.py
@@ -4,18 +4,25 @@ import matplotlib.pyplot as plt
 import datetime
 import pandas as pd
 
-data = pd.read_csv('weather_data.csv',parse_dates=True)
-x = pd.to_datetime(data['date-time'])
-t = data['temperature']
-h = data['humidity']
-plt.xlabel('Real-Time') 
-plt.ylabel('Value') 
-plt.title('Title')
-plt.plot(x, t)
-plt.plot(x, h)
-plt.legend(["Temperature (c)", "Humidity (%)"]);
+def parseArgs(args):
+    return None
 
-plt.gcf().autofmt_xdate()
-plt.show()
+def generateGraph(data_file='weather_data.csv',output_figure='images/weather.png'):
+    data = pd.read_csv(data_file,parse_dates=True)
+    x = pd.to_datetime(data['date-time'])
+    t = data['temperature']
+    h = data['humidity']
+    plt.xlabel('Real-Time') 
+    plt.ylabel('Value') 
+    plt.title('Title')
+    plt.plot(x, t)
+    plt.plot(x, h)
+    plt.legend(["Temperature (c)", "Humidity (%)"]);
 
-plt.savefig('images/weather.png')
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+    plt.savefig(output_figure)
+
+if __name__ == "__main__":
+    generateGraph()
